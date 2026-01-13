c44b4d0009446f931f315cdf71acad7e431e897f
alecwoods <alecwoods@aol.com>
2026-01-13 21:42:53 +0000

    Added docstring to load_to_database function

--- source/etl/load.py
+++ source/etl/load.py
@@ -3,7 +3,13 @@ import pandas as pd # pyright: ignore[reportMissingModuleSource]
 from source.db.connection import get_engine
 
 def load_to_database(df: pd.DataFrame, table_name: str):
-
+    """
+    Loads a given DataFrame into PostgreSQL database.
+    
+    Params:
+        df (pd.DataFrame): DataFrame to be loaded into database.
+        table_name (str): Name of target table in database
+    """
     engine = get_engine()
 
     df.to_sql(table_name, engine, if_exists='replace', index=False)
