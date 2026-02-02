116de4120d5a2cd5c9c2e08baabcff82ce5c2f46
Alec Woods <alecwoods@aol.com>
2026-02-02 18:15:26 +0000

    Remove redundant code from streamlit app

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,12 +1,4 @@
 import streamlit as st
-import sys
-import os
-
-# Add the project root to sys.path
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-if project_root not in sys.path:
-    sys.path.insert(0, project_root)
-
 import pandas as pd # pyright: ignore[reportMissingModuleSource]
 from source.db.connection import query_data
 
