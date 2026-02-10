9640491d0e0fe4a6f2a44913158ea9f103c964ae
Alec Woods <alecwoods@aol.com>
2026-02-10 16:40:23 +0000

    Add last succesful update time to streamlit app

--- streamlit/app.py
+++ streamlit/app.py
@@ -8,9 +8,22 @@ st.set_page_config(
 
 st.title('Leaderboard')
 
-query = "SELECT * FROM serving.leaderboard"
+leaderboard_query = "SELECT * FROM serving.leaderboard"
 
-df = query_data(query, user='app')
-df.drop(columns=['player_id'], inplace=True)
+df_mvp = query_data(leaderboard_query, user='app')
+df_mvp.drop(columns=['player_id'], inplace=True)
 
-st.dataframe(df, use_container_width=True, hide_index=True)
+st.dataframe(df_mvp, use_container_width=True, hide_index=True)
+
+update_query = """
+    SELECT end_time
+    FROM metadata.update_runs
+    WHERE status='success'
+    ORDER BY end_time DESC
+    LIMIT 1
+"""
+
+df_update = query_data(update_query, user='app')
+last_update = pd.to_datetime(df_update['end_time'][0]).date()
+
+st.caption(f"Last updated: {last_update}")
