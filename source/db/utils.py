0562086e8ae812137270d8faff57f68810f77c6b
Alec Woods <alecwoods@aol.com>
2026-02-10 16:41:22 +0000

    Remove create_serving_view function and all references
    Serving view is now defined on database initialization

--- source/db/utils.py
+++ source/db/utils.py
@@ -21,21 +21,3 @@ def remove_season_data(season: int):
         """
 
         conn.execute(query)
-
-
-def create_serving_view():
-    """Creates a view in PostgreSQL database that queries player stats for players with predicted vote share above 0."""
-
-    SQL_FILE_PATH = "/opt/airflow/sql/serving.sql"
-
-    sql_path = pathlib.Path(SQL_FILE_PATH)
-
-    if not sql_path.exists():
-        raise FileNotFoundError(f"SQL file not found: {SQL_FILE_PATH}")
-    
-    sql = sql_path.read_text()
-
-    engine = get_engine(user='ml')
-
-    with engine.begin() as conn:
-        conn.execute(sql)
