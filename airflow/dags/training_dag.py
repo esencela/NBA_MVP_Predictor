8e9b662625b437ef3f0047b1a4c853b0a66f3707
Alec Woods <alecwoods@aol.com>
2026-02-17 16:39:07 +0000

    Remove loading predictions from ml_dag
    For seperation of concerns, this dag will only train the model. Future updates should add logging and evaluation tasks. Also renamed dag to fit changes.

---
+++ airflow/dags/training_dag.py
@@ -0,0 +1,42 @@
+from airflow.decorators import dag, task # pyright: ignore[reportMissingImports]
+from datetime import datetime, timedelta
+from source.ml.train import train_model
+from source.ml.predict import get_predictions
+from source.etl.load import load_to_database
+import pandas as pd # pyright: ignore[reportMissingModuleSource]
+from pathlib import Path
+import shutil
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'retries': 0,
+    'retry_delay': timedelta(minutes=5)
+}
+
+@dag(
+    dag_id='model_training_dag',
+    default_args=default_args,
+    description='ML DAG that trains model on historic data',
+    schedule_interval=None,
+    start_date=datetime(2026, 1, 18),
+    catchup=False
+)
+def ml_pipeline():
+    """
+    ML Pipeline DAG for NBA MVP Predictor project.
+
+    This DAG trains the machine learning model using the features dataset in Postgres.
+    """
+
+    @task
+    def train():
+        """Trains the machine learning model."""
+
+        train_model()
+
+
+    train()
+
+
+ml_dag = ml_pipeline()
