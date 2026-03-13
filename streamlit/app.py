0be3f80024e6da5c687f8b58c976f4a147cbb0cd
Alec Woods <alecwoods@aol.com>
2026-03-13 17:30:25 +0000

    Polish streamlit dashboard
    Dashboard now has player headshots and team logos in the dataframe and displays the predicted MVP above the leaderboard dataframe

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,12 +1,11 @@
 import streamlit as st
 import pandas as pd # pyright: ignore[reportMissingModuleSource]
 from source.db.connection import query_data
+from source.config.settings import CURRENT_SEASON
 
 st.set_page_config(
     page_title="NBA MVP Predictor"
 )
-
-st.title('Leaderboard')
 
 leaderboard_query = """
     SELECT 
@@ -18,9 +17,61 @@ leaderboard_query = """
 """
 
 df_mvp = query_data(leaderboard_query, user='app')
-df_mvp.drop(columns=['player_id'], inplace=True)
 
-st.dataframe(df_mvp, use_container_width=True, hide_index=True)
+# Add headshot URLs based on player_id
+df_mvp['Headshot'] = df_mvp['player_id'].apply(
+    lambda x: f'https://basketball-reference.com/req/202212091/images/headshots/{x}.jpg'
+)
+
+df_mvp['Team_Logo'] = df_mvp['Team'].apply(
+    lambda x: f'https://cdn.ssref.net/req/202603120/tlogo/bbr/{x}-{CURRENT_SEASON}.png'
+)
+
+df_mvp.drop(columns=['player_id', 'Team'], inplace=True)
+
+df_mvp = df_mvp[['Rank', 'Headshot', 'Player', 'Team_Logo', 'MP','PTS', 'AST', 'TRB', 'STL', 'BLK', 'Predicted_Share']]
+
+st.title('NBA MVP Predictor :basketball:')
+st.markdown('Machine learning model to predict the NBA MVP based on player stats and team performance.')
+st.divider()
+
+leader = df_mvp.iloc[0]
+
+col1, col2 = st.columns([2, 1])
+
+with col1:
+    st.metric(
+        label=f'Current MVP Favourite:',
+        value=leader['Player'],
+        delta=f"{leader['Predicted_Share']:.3f} MVP Share"
+    )
+    
+with col2:
+    st.image(leader['Headshot'], width=100)
+
+
+
+st.subheader('MVP Leaderboard')
+
+st.dataframe(
+    df_mvp, 
+    column_config={
+        'Headshot': st.column_config.ImageColumn(
+            '',
+            width=50
+        ),
+        'Team_Logo': st.column_config.ImageColumn(
+            'Team',
+            width=50
+        ),
+        'Predicted_Share': st.column_config.NumberColumn(
+            'Vote Share',
+            format='%.3f'
+        )
+    },
+    use_container_width=True, 
+    hide_index=True
+)
 
 update_query = """
     SELECT data_freshness
@@ -32,4 +83,4 @@ update_query = """
 df_update = query_data(update_query, user='app')
 data_freshness = pd.to_datetime(df_update['data_freshness'][0]).date()
 
-st.caption(f"Stats last updated: {data_freshness}")
+st.caption(f':clock2: Stats last updated: {data_freshness}')
